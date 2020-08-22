0a3cc3b9bdbe1e95eba3351110815c2dc74f54b7
vinace <p.vinsentds@gmail.com>
2020-08-23 00:48:00 +0530

    Added some clearning memory steps torch.cuda.clear_memory_allocated()

--- seq_sum.py
+++ seq_sum.py
@@ -175,7 +175,8 @@ class MultiHeadAttentionLayer(nn.Module):
         #V = [batch size, n heads, value len, head dim]
                 
         # energy = torch.matmul(Q, K.permute(0, 1, 3, 2)) / self.scale
-        energy = torch.matmul(Q, K.transpose(1,0)) / self.scale
+        Q *= 0.06
+        energy = torch.matmul(Q, K.transpose(1,0)) #  / self.scale
         
         #energy = [batch size, n heads, query len, key len]
         
