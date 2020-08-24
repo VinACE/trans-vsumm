135dd424b237dc7f726a726698251114dd6b2eb1
vinace <p.vinsentds@gmail.com>
2020-08-24 15:33:52 +0530

    changed the mask values

--- seq_sum.py
+++ seq_sum.py
@@ -402,7 +402,7 @@ class Seq2Seq(nn.Module):
         # src_mask = (src != self.src_pad_idx).unsqueeze(1).unsqueeze(2)
 
         # #src_mask = [batch size, 1, 1, src len]
-        self.apperture = 1  # 1 -2 = 1 # TODO Need to understand the mask values better..
+        self.apperture = 2  # 1 -2 = 1 # TODO Need to understand the mask values better..
 
         onesmask = torch.ones(n, n)
         src_mask = torch.tril(onesmask, -self.apperture) + torch.triu(onesmask, self.apperture)
