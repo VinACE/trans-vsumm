3af86dfb24502bd0a27c397023bf97b24cfc1299
vinace <p.vinsentds@gmail.com>
2020-08-30 01:37:28 +0530

    changed the feature size to 512 from 1024 as input size

--- main.py
+++ main.py
@@ -279,7 +279,7 @@ class AONet:
                 seq = dataset['features'][...]
                 print(f'shape of seq befor unsequeeze {seq.shape}') # TODO remove the prints for shape
                 print(f'size of seq befor unsequeeze {seq.size}')
-                print(f'type of seq is ' {type(seq)}')
+                print(f'type of seq is {type(seq)}')
                 seq = torch.from_numpy(seq).unsqueeze(0)
                 seq = torch.narrow(seq,1,0,512)
                 target = dataset['gtscore'][...]
